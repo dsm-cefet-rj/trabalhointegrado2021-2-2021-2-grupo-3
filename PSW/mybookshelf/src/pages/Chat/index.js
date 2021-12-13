2c1cddec0b27cbdbb669b999ce02c7f98719fbc7
Luis <cfp5wls@hotmail.com>
2021-12-13 17:36:55 -0300

    css chat

--- PSW/mybookshelf/src/pages/Chat/index.js
+++ PSW/mybookshelf/src/pages/Chat/index.js
@@ -7,6 +7,11 @@ export default function Chat(){
         <>
         <Container_chat img={pingu} msg="mensagem teste" hora="11:30"/>
         <Container_chat msg="mensagem teste"/>
+        <footer>
+			<textarea style="width: 100%;" placeholder="Type your message"></textarea>
+		</footer>
+        <i style="font-size: 2.2em;" class="bi bi-images"></i>
+        <a style="transform: translate(20px, -7px);" href="#" class= "btn btn-primary">Enviar</a>
         </>
     );
 }
