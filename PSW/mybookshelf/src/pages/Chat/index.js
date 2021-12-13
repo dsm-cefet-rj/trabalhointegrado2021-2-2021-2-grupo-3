5f8abac8d8a27ebcfecf9fe2ad2a831bbda8bc4e
Brigside <2000fellipe@gmail.com>
2021-12-13 18:15:39 -0300

    css chat

--- PSW/mybookshelf/src/pages/Chat/index.js
+++ PSW/mybookshelf/src/pages/Chat/index.js
@@ -8,7 +8,7 @@ export default function Chat(){
         <Container_chat className="container"img={pingu} msg="mensagem teste" hora="11:30"/>
         <Container_chat className="container darker"msg="mensagem teste"/>
         <Container_chat className="container darker"msg="mensagem teste155000"/>
-        <Container_chat className="container darker" msg="vai">
+        <Container_chat className="container darker" msg="vai"/>
 		<textarea placeholder="Type your message"></textarea>
         <i class="bi bi-images"></i>
         <a href="#" class= "btn btn-primary">Enviar</a>
