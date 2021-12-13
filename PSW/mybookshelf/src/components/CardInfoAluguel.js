64d091fac361dcd17adde1e5101502e4b2eafe49
Brigside <2000fellipe@gmail.com>
2021-12-13 16:15:56 -0300

    Criação do CardInfoAluguel

---
+++ PSW/mybookshelf/src/components/CardInfoAluguel.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default (props) => {
+    return (
+        <div className={props.className}>
+          <div class="card-img">
+              <img src={props.img} class="book-img"/>
+          </div>
+          <div class="card-body">
+            <h3 class="card-title">{props.title}</h3>
+            <div class="card-description">
+              <h5>Descrição:</h5>
+              <p>
+                {props.descricao}
+              </p>
+            </div>
+                  
+            <div class="rent-information">
+              <p class="rent-date"><strong>Alugado em:</strong> {props.dataAluguel}</p>
+              <p><strong>Proprietário:</strong>{props.proprietario}</p>
+            </div>
+          </div>
+        </div> 
+    );
+}
--- PSW/mybookshelf/src/pages/DevoluçãoLivro/index.js
+++ PSW/mybookshelf/src/pages/DevoluçãoLivro/index.js
@@ -1,16 +1,35 @@
 import React from "react";
+import CardInfoAluguel from "../../components/CardInfoAluguel";
 import Input  from "../../components/Input";
+import livro from '../../img/livro4.jpg'
 
 export default function DevoluçãoLivro() {
     return(
         <div>
-            <Input 
-                className="half-box spacing"
-                id="dataDevolução"
-                label="Agendar Devolução"
-                placeholder="Digite a Data para a Devolução"
-                type="date"
+            <h1> Marcar Devolução </h1>
+            <CardInfoAluguel
+                img={livro}
+                title="O Poder do Hábito" 
+                descricao="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
+                nventore! Iusto, at. Sint, eum! Laborum nostrum expedita odit! 
+                Distinctio quaerat sed temporibus, optio mollitia porro quia molestias 
+                doloribus harum eius."
+                dataAluguel="12/12/2021" 
+                proprietario="Junin da Maçã"
             />
+            <form id="devolution-form">
+                <Input 
+                    className="half-box spacing"
+                    id="dataDevolução"
+                    label="Agendar Devolução"
+                    placeholder="Digite a Data para a Devolução"
+                    type="date"
+                />
+                <div class="full-box">
+                    <input type="submit" value="registrar" id="btn-submit"/>
+                </div>
+            </form>
+            
         </div>
     );
 };
