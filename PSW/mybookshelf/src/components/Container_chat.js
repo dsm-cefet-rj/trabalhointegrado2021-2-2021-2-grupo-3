c767b5a284fbe56d1503948e23c045123a7690d3
Luis <cfp5wls@hotmail.com>
2021-12-13 17:29:26 -0300

    css chat

--- PSW/mybookshelf/src/components/Container_chat.js
+++ PSW/mybookshelf/src/components/Container_chat.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "../css/styles_text"
 
 export default(props) => {
     return(
         <>
-        <link  rel="stylesheet" href="../css/styles_text.css"/>
+        <link  rel="stylesheet" href="css/styles_text.css"/>
         <div className={props.className}>
         <img src={props.img} alt={"Avatar"}/>
         <p>{props.msg}</p>
