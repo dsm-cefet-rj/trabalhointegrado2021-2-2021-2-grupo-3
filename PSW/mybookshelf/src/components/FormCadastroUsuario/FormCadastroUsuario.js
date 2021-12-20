7d39b61536e91a78640f9fc6215a678f35aaf4b1
João Victor Rangel <55825561+jvmrangel@users.noreply.github.com>
2021-12-20 07:30:47 -0300

    Forms Hook Var. local
    Ainda não foi implementado uma maneira de salvar o objeto usuário.

--- PSW/mybookshelf/src/components/FormCadastroUsuario/FormCadastroUsuario.js
+++ PSW/mybookshelf/src/components/FormCadastroUsuario/FormCadastroUsuario.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../Input";
 import './FormCadastroUsuario.css';
 import {
@@ -10,6 +10,24 @@ import {
 
 
 export default () => {
+
+    const [formValues, setFormValues] = useState({});
+
+    const handleInputChange = (e) => {
+        const { name, value} = e.target;
+        setFormValues({...formValues, [name]: value });
+    };
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+
+        console.log('*** handleSubmit', data);
+    };
+
+    console.log('**** formValues', formValues);
+
     return(
         <div>
             <meta charSet="UTF-8" />
@@ -17,73 +35,96 @@ export default () => {
             <link rel="stylesheet" href="css/styles_cadastro_livros.css" />
             <div id="main-container">
                 <h1> Criar nova conta </h1>
-                <form id="regiter-form">
-                <Input 
-                    className="full-box"
-                    id="nomeUsuario"
-                    label="Nome do Usuário"
-                    placeholder="Digite o nome do usuário"
-                    type="nomeUsuario"
-                />    
-                <Input 
-                    className="full-box"
-                    id="apelidoUsuario"
-                    label="Apelido"
-                    obs="Como aparecerá nos seus anúncios. (obrigatório)"
-                    placeholder="Digite seu apelido" 
-                    type="apelidoUsuario"
-                />
-                <Input 
-                    className="half-box spacing"
-                    id="cpfUsuario"
-                    label="CPF do usuário"
-                    placeholder="999.999.999-99" 
-                    required="required"
-                    type="cpfUsuario"
-                />
-                <Input 
-                    className="half-box"
-                    id="celularUsuario"
-                    label="Número do celular"
-                    placeholder="(99) 9999-9999" 
-                    pattern="(\([0-9]{2}\))\s([9]{1})?([0-9]{5})-([0-9]{4})"
-                    title="Número de telefone precisa ser no formato (99) 99999-9999"
-                    required="required"
-                    type="celularUsuario"
-                />
+                <form onSubmit={handleSubmit}  id="regiter-form">
+                <div className="full-box">
+                    <label htmlFor="nomeUsuario">Nome do Usuário</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="nomeUsuario"
+                        placeholder="Digite o nome do usuário"
+                        onChange={handleInputChange}
+                        value={formValues.name || ''}
+                    />
+                </div>
+                <div className="full-box">
+                    <label htmlFor="apelidoUsuario">Apelido</label><div className="obs">Como aparecerá nos seus anúncios. (obrigatório) </div>
+                    <input 
+                        type="text" 
+                        name="nickname" 
+                        placeholder="Digite seu apelido" 
+                        onChange={handleInputChange}
+                        value={formValues.nickname || ''}
+                    />
+                </div>
+                <div class="half-box spacing">
+                    <label for="cpfUsuario">CPF do usuário</label>
+                    <input
+                        type="cpf" 
+                        name="cpf"  
+                        placeholder="999.999.999-99"
+                        onChange={handleInputChange }
+                        value={formValues.cpf || ''}
+                    />
+                </div>
+                <div class="half-box">
+                    <label htmlFor="celularUsuario">Número do celular<span class="required"></span></label>
+                    <input
+                        type="cel"
+                        name="cel"
+                        placeholder="(99) 9999-9999" 
+                        //pattern="(\([0-9]{2}\))\s([9]{1})?([0-9]{5})-([0-9]{4})" 
+                        title="Número de telefone precisa ser no formato (99) 99999-9999" 
+                        required="required"
+                        onChange={handleInputChange}
+                        value={formValues.cel || ''} 
+                    />   
+                </div>
+
                     {/* Celular em Full box
                         <div className="full-box">
                             <label htmlFor="celularUsuario">Número do celular<span class="required"></span></label>
                             <input type="celularUsuario" name="celularUsuario" id="celularUsuario" placeholder="(99) 9999-9999" pattern="(\([0-9]{2}\))\s([9]{1})?([0-9]{5})-([0-9]{4})" title="Número de telefone precisa ser no formato (99) 99999-9999" required="required" />
                         </div>
                     */}
-                <Input 
-                    className="full-box"
-                    id="emailUsuario"
-                    label="Email"
-                    placeholder="Digite seu email"
-                    type="emailUsuario"
-                />
-                <Input 
-                    className="full-box"
-                    id="senhaUsuario"
-                    label="Senha"
-                    placeholder="Digite sua senha"
-                    type="password"
-                />
-                <Input 
-                    className="full-box"
-                    id="confirmacaoSenha"
-                    label="Confirmar senha"
-                    placeholder="Confirme sua senha"
-                    type="password"
-                />
+                <div className="full-box">
+                    <label htmlFor="emailUsuario">Email</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Digite seu email" 
+                        onChange={handleInputChange}
+                        value={formValues.email || ''}
+                    />
+                </div>
+                <div className="full-box">
+                    <label htmlFor="senhaUsuario">Senha</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Digite sua senha"
+                        onChange={handleInputChange}
+                        value={formValues.password || ''}
+                    />
+                </div>
+                <div className="full-box">
+                    <label htmlFor="senhaUsuario">Confirmar senha</label>
+                    <input 
+                        type="password" 
+                        name="confirmacaoPassword" 
+                        placeholder="Confirme sua senha" 
+                    />
+                </div>
                 <div className="full-box">
                     <input type="checkbox" name="agreement" id="agreement" />
                     <label htmlFor="agreement" id="agreement-label">Eu li e aceito os <a href="#">termos de uso</a></label>
                 </div>
                 <div className="full-box">
-                    <Link to="/"><input type="submit" defaultValue="registrar" id="btn-submit" /></Link>                
+                    <input 
+                        type="submit" 
+                        defaultValue="registrar" 
+                        id="btn-submit" 
+                    />
                 </div>
                 </form>
             </div>
