5595c08deddef9ea9b4f45bc7ea83e8ec336047a
Luis <cfp5wls@hotmail.com>
2021-12-21 19:59:54 -0300

    mais chat blabla

--- PSW/mybookshelf/src/pages/Chat/index.js
+++ PSW/mybookshelf/src/pages/Chat/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import Container_chat from "../../components/Container_chat";
 import pingu from "../../img/pingu.jpg";
 import rino from "../../img/rino.jpg";
 import hipo from "../../img/hipo.png";
 import sapin from "../../img/sapin.jpg";
+
 
 const var_chat = [{
     className: "container",
@@ -31,12 +32,20 @@ const var_chat = [{
 }]
 
 export default function Chat(){
+    function FChat(e) {
+        e.preventDefault()
+        const chat_data = new FormData(e.target)
+        const ref_chat_data = Object.fromEntries(chat_data)
+        console.log(ref_chat_data)
+    }
     return(
         <div className="chat">
         <Container_chat chat={var_chat}/>
-		<textarea placeholder=""></textarea>
-        <i class="bi bi-images"></i>
-        <a href="#" class= "btn btn-primary">Enviar</a>
+        <form onSubmit={(e) => {FChat(e)}}>
+            <textarea placeholder="" name="mensagem"></textarea>
+            <i class="bi bi-images"></i>
+            <input type="submit" href="#" class= "btn btn-primary" value="Enviar"/>
+        </form>
         </div>
     );
 }
