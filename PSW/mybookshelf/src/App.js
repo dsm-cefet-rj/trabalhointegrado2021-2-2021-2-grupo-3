244a418a2d72a35260f2693c64963e26ea632c56
Nicholas Capone <nicholas.capone@reserve.com.br>
2022-01-19 18:00:18 -0300

    Inicio da migração da aplicação para usar o EntityAdapter

--- PSW/mybookshelf/src/App.js
+++ PSW/mybookshelf/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import Routes from './routes';
+
 
 function App() {
     return ( 
--- PSW/mybookshelf/src/store/slices/cadastroLivroSlice.js
+++ PSW/mybookshelf/src/store/slices/cadastroLivroSlice.js
@@ -1,6 +1,13 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {createAsyncThunk, createEntityAdapter, createSlice} from '@reduxjs/toolkit'
 import livro3 from '../../img/livro3.jpg'
 import livro4 from '../../img/livro4.jpg'
+
+const livrosAdapter = createEntityAdapter();
+
+const initialState = livrosAdapter.getInitialState({
+    status: 'not_loaded',
+    error: null
+});
 
 const livrosIniciais = [{
     titulo: "O poder do hábito",
@@ -32,6 +39,25 @@ export const cadastroLivroSlice = createSlice({
         removerLivro: (state, action) => removerLivroReducer(state, action.payload)
     }
 })
+
+//Migrando a aplicação para utilizar o EntityAdapter - Passando os dados para Async Thunk
+//Aqui joga para o servidor e se for ok, vai atualizar na tela
+export const updateLivrosServer = createAsyncThunk('livros/updateLivrosServer', 
+    async (livro) => {
+        let response = await fetch('http://localhost:3000/livros/' + livro.id , 
+        {
+            method: 'PUT', //Pode ser post para adição ou DELETE para remoção
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(livro)
+        });
+        if(response.ok){
+            return livro;
+        } else {
+            throw new Error("Erro ao atualizar o livro");
+        }
+    });
 
 export const { cadastrarLivro, removerLivro } = cadastroLivroSlice.actions
 export default cadastroLivroSlice.reducer
