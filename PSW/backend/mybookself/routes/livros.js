49b7e3c2e0f5151b0998e9ca9e0acd082dff2093
Brigside <2000fellipe@gmail.com>
2022-01-26 14:46:48 -0300

    Criação da rota de Alugados

--- PSW/backend/mybookself/routes/livros.js
+++ PSW/backend/mybookself/routes/livros.js
@@ -4,6 +4,14 @@ var router = express.Router();
 const fs = require('fs')
 var livrosJson = require('../bdLocal/livros.json')
 
+function SalvarJson (livrosJson) {
+  const data = JSON.stringify(livrosJson) // Converte o Json 
+  fs.writeFile('./bdLocal/livros.json', data, (err) => { // sobrescreve o arquivo livros.json 
+    if (err) throw err;
+    console.log('Livro Cadastro no livros.json');
+  })
+}
+
 /* GET informações dos livros. */
 router.get('/', (req, res, next) => {
   res.status(200).send(livrosJson);
@@ -11,18 +19,24 @@ router.get('/', (req, res, next) => {
 
 router.post('/cadastro', (req, res, next) => {
   livrosJson.livros.push(req.body) 
-  
-  const data = JSON.stringify(livrosJson) // Converte o Json 
-  fs.writeFile('./bdLocal/livros.json', data, (err) => { // sobrescreve o arquivo livros.json 
-    if (err) throw err;
-    console.log('Livro Cadastro no livros.json');
-  })
-  
+  SalvarJson(livrosJson)
   res.status(200).send("Livro cadastrado")
 })
 
-router.delete('/Alugado', (req, res, next) => {
-  res.send("não funcionado ainda")
+router.post('/Alugado', (req, res, next) => {
+  const { idLivro, idPropietario, idLocatario } = req.body
+  const data = {
+    idAluguel: Math.floor(Math.random() * 100000),
+    idLivro, 
+    idPropietario, 
+    idLocatario
+  }
+
+  livrosJson.Alugados.push(data)
+  
+  SalvarJson(livrosJson)
+  
+  res.send("livro Alugado")
 })
 
 module.exports = router;
