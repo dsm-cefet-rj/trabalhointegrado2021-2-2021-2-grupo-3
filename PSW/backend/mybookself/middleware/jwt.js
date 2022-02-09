76fcba34417d1b351f0cd6c64e9e246f103c3a38
Brigside <2000fellipe@gmail.com>
2022-02-09 18:33:53 -0300

    minhas mudanças

---
+++ PSW/backend/mybookself/middleware/jwt.js
@@ -0,0 +1,3 @@
+const jwt = require('jsonwebtoken')
+const express = require('express');
+const router = express.Router();
--- PSW/backend/mybookself/routes/login.js
+++ PSW/backend/mybookself/routes/login.js
@@ -1,22 +1,65 @@
 const express = require('express');
 const router = express.Router();
 const usuario = require('../models/usuarioModel') 
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
-router.post('/', async (req, res) => {
-    const {email, password} = req.body
+router.post('/', (req, res, next) => {
+    if(!req.session.user) {
+        var authHeader = req.headers.authorization;
 
-    const user = await usuario.findOne({email})
-    if(!user) {
-        return res.status(401).send("Usuario ou Senha invalidos!!")
+        /* if(!authHeader) {
+            var err = new Error(' Você não está autorizado!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+
+        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var username = auth[0];
+        var password = auth[1]; */
+
+        const {email, password} = req.body
+
+        usuario.findOne({email})
+        .then((user) => {
+            console.log(user)
+            if(user === null) {
+                var err = new Error('Email invalido!!');
+                err.status = 403;
+                return next();
+            }
+            else if (user.password !== password) {
+                var err = new Error('Sua senha está incorreta!');
+                err.status = 403;
+                return next(err);
+            }
+            else if (user.email === email && user.password === password) {
+                req.session.user = 'authenticated';
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Você está autenticado!')
+            }
+        })
+        .catch((err) => next(err));
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Você já está autenticado!');
     }
+    })
 
-    if(password == user.password) {
-        req.session.user = user._id
-        console.log(user._id)
-        res.status(200).end("usuario logado")
-    } else 
-        res.status(401).send("Usuario ou Senha invalidos!!")
-})
+router.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy();
+        res.clearCookie('session-id');
+        res.send('logout!!')
+    } else {
+        var err = new Error('Você não está logado!');
+        err.status = 403;
+        next(err);
+    }
+});
 
 
 module.exports = router
