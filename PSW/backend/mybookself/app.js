0022f957352572e84ed326d22789105b25a4ebf6
Nicholas Capone <nicholas.capone@reserve.com.br>
2022-02-09 16:42:44 -0300

    Sessoes, cookies e validação de login

--- PSW/backend/mybookself/app.js
+++ PSW/backend/mybookself/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var cors = require('cors');
 
 const mongoose = require('mongoose')
@@ -24,7 +26,54 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-67890-73567-54321'));
+
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-73567-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
+
+function auth (req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        var authHeader = req.headers.authorization;
+        if(!authHeader) {
+            var err = new Error('Você não está autorizado!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+            return;
+        }
+
+        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if (user == 'admin' && pass == 'password') {
+            req.session.user = 'admin';
+            next();
+        } else {
+            var err = new Error('Você não está autorizado!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+        }
+    } else {
+        if (req.session.user === 'admin') {
+            console.log('req.session: ',req.session);
+            next();
+        } else {
+            var err = new Error('Você não está autorizado!');
+            err.status = 401;
+            next(err);
+        }
+    }
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
